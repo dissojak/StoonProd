da821dd12ebdd443bd16fa7ad957fe21331bdd13
Adem Ben Amor <dissojak@gmail.com>
2024-12-22 07:03:41 +0100

    adjust converstion from css > tailwind lg:xs

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -8,6 +8,7 @@ const Navbar = () => {
   const pathname = usePathname(); // Get the current path
   const [activeLink, setActiveLink] = useState("");
   const [isDarkMode, setIsDarkMode] = useState(false); // State for dark mode
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for mobile menu toggle
 
   useEffect(() => {
     setActiveLink(pathname);
@@ -17,6 +18,11 @@ const Navbar = () => {
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode); // Toggle dark mode
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle mobile menu
+    console.log("Toggle Menu");
   };
 
   useEffect(() => {
@@ -29,92 +35,86 @@ const Navbar = () => {
 
   return (
     <Fragment>
-      <nav className="flex items-center justify-between 
-      lg:px-24 lg:py-7.5 text-white 
-      xs:px-4 xs:py-2">
+      <nav className="flex items-center justify-between px-4 py-2 xs:px-6 xs:py-4 lg:px-24 lg:py-7.5 text-white">
         <Link href="/" className="flex-shrink-0">
           <img
             src="./assets/images/logostoonprod_rec_white.png"
             alt="Logo"
-            className="relative lg:h-16 lg:mt-auto xs:h-10 xs:-mt-10"
+            className="relative lg:h-16 lg:mt-auto xs:h-12"
           />
         </Link>
-        <ul className="flex space-x-4">
-          <li className="list-none inline-block px-5 py-2.5">
-            <Link
-              href="/"
-              className={`relative py-1.5 hover:text-myRed after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-myRed after:transition-all after:duration-300 hover:after:w-full ${
-                isActive("/") ? "text-myRed font-bold" : ""
-              }`}
-            >
-              Home
-            </Link>
-          </li>
-          <li className="list-none inline-block px-5 py-2.5">
-            <Link
-              href="/about"
-              className={`relative py-1.5 hover:text-myRed after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-myRed after:transition-all after:duration-300 hover:after:w-full ${
-                isActive("/about") ? "text-myRed font-bold" : ""
-              }`}
-            >
-              About
-            </Link>
-          </li>
-          <li className="list-none inline-block px-5 py-2.5">
-            <Link
-              href="/services"
-              className={`relative py-1.5 hover:text-myRed after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-myRed after:transition-all after:duration-300 hover:after:w-full ${
-                isActive("/services") ? "text-myRed font-bold" : ""
-              }`}
-            >
-              Service and Tariffs
-            </Link>
-          </li>
-          <li className="list-none inline-block px-5 py-2.5">
-            <Link
-              href="/ourClients"
-              className={`relative py-1.5 hover:text-myRed after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-myRed after:transition-all after:duration-300 hover:after:w-full ${
-                isActive("/ourClients") ? "text-myRed font-bold" : ""
-              }`}
-            >
-              Clients
-            </Link>
-          </li>
-          <li className="list-none inline-block px-5 py-2.5">
-            <Link
-              href="/blog"
-              className={`relative py-1.5 hover:text-myRed after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-myRed after:transition-all after:duration-300 hover:after:w-full ${
-                isActive("/blog") ? "text-myRed font-bold" : ""
-              }`}
-            >
-              Blog
-            </Link>
-          </li>
-          <li className="list-none inline-block px-5 py-2.5">
-            <Link
-              href="/contact"
-              className={`relative py-1.5 hover:text-myRed after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-myRed after:transition-all after:duration-300 hover:after:w-full ${
-                isActive("/contact") ? "text-myRed font-bold" : ""
-              }`}
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+
         <div className="flex space-x-4">
           <Link
             href="/resume"
-            className="relative bg-myYellow text-black px-6 py-2 rounded-full hover:bg-yellow-300 dark:bg-yellow-500 dark:text-white"
+            className="relative bg-myYellow text-black lg:px-6 xs:px-3 py-2 rounded-full hover:bg-yellow-300 dark:bg-yellow-500 dark:text-white"
           >
             Resume
           </Link>
           <button
             onClick={toggleDarkMode}
-            className="relative bg-myRed text-white px-6 py-2 rounded-full hover:bg-red-300 dark:bg-red-700 dark:text-gray-200"
+            className="relative bg-myRed text-white lg:px-6 xs:px-3 py-2 rounded-full hover:bg-red-300 dark:bg-red-700 dark:text-gray-200"
           >
             {isDarkMode ? "Light Mode" : "Dark Mode"}
           </button>
         </div>
+        {/* Hamburger Menu Icon (Only visible on xs screens) */}
+        <button
+          onClick={() => {
+            toggleMenu(); // Call the toggleMenu function
+            console.log("Toggle Menu"); // Log a message to the console
+          }}
+          className="lg:hidden text-white focus:outline-none relative "
+        >
+          <svg
+            className="h-8 w-8"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+
+        {/* Full menu for lg screens, collapsible for xs screens */}
+        <ul
+          className={`lg:flex space-x-4 xs:${
+            isMenuOpen ? "block" : "hidden"
+          } xs:space-y-2 lg:space-y-0`}
+        >
+          {[
+            "Home",
+            "About",
+            "Service and Tariffs",
+            "Clients",
+            "Blog",
+            "Contact",
+          ].map((item, idx) => (
+            <li
+              key={idx}
+              className="list-none inline-block lg:px-5 lg:py-2.5 xs:px-2 xs:py-2"
+            >
+              <Link
+                href={`/${item.toLowerCase().replace(/\s+/g, "")}`}
+                className={`relative py-1.5 hover:text-myRed after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[3px] after:bg-myRed after:transition-all after:duration-300 hover:after:w-full ${
+                  isActive(`/${item.toLowerCase()}`)
+                    ? "text-myRed font-bold"
+                    : ""
+                }`}
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+
       </nav>
     </Fragment>
   );
--- src/app/components/SliderSection.js
+++ src/app/components/SliderSection.js
@@ -8,11 +8,11 @@ const SliderSection = () => {
       md:flex-row items-center justify-between 
       lg:py-16 xs:py-0
       ">
-        <div className="w-1/2">
+        <div className="lg:w-1/2 xs:w-full">
           <img
             src="./assets/images/filmingGreen.svg"
             alt="website template image"
-            className="w-full mx-auto"
+            className="lg:w-full mx-auto xs:mt-4 xs:w-10/12"
           />
         </div>
         <div className="detail-box md:w-1/2 text-white text-right ">
@@ -26,15 +26,19 @@ const SliderSection = () => {
               stunning photography, and cutting-edge content creation. <br />
               We craft stories that resonate and websites that inspire.
             </p>
-            <div className="relative flex justify-center">
-              <div className="bg-myGreenDarken p-1 rounded-full w-1/3 pr-10">
+            <div className="relative flex justify-center xs:mt-16">
+              <div className="bg-myGreenDarken p-1 rounded-full lg:w-1/3 lg:pr-10 xs:pr-[4.5rem] xs:w-2/3">
                 <Link
                   href="https://www.instagram.com/adem_ben_amor/"
-                  className="inline-flex px-8 py-4 bg-myYellow text-black rounded-full 
+                  className="inline-flex lg:px-8 lg:py-4 bg-myYellow text-black rounded-full 
                   transition-transform
                   hover:translate-x-9 hover:bg-myYellow
                   hover:text-white hover:font-medium
-                  hover:inline-flex"
+                  hover:inline-flex
+                  
+                  xs:px-4
+                  xs:py-4
+                  xs:hover:translate-x-[4.25rem]"
                 >
                   Contact Us
                   <div className="arrow-container">
