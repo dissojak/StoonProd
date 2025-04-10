b755b78c902675209f297d8f1a6d45487512e954
dissojak <dissojak@gmail.com>
2025-04-10 21:29:51 +0100

    edit slider of clients

--- src/app/UI/LogoSlider.js
+++ src/app/UI/LogoSlider.js
@@ -2,163 +2,56 @@ import React from "react";
 
 const LogoSlider = () => {
   return (
-    <>
-      <div className="relative overflow-hidden py-6 bg-violet-500 -mt-9 rotate-[-2.5deg] transform skew-x-[-2.5deg] w-full">
-        {/* Gradient mask for the left and right edges */}
-        <div className="absolute top-0 left-0 h-full w-32 sm:w-48 bg-gradient-to-l from-transparent to-white dark:to-black z-10"></div>
-        <div className="absolute top-0 right-0 h-full w-32 sm:w-48 bg-gradient-to-r from-transparent to-white dark:to-black z-10"></div>
+    <div className="relative overflow-hidden py-6 bg-violet-500 -mt-9 rotate-[-2.5deg] transform skew-x-[-2.5deg] w-full">
+      <div className="absolute top-0 left-0 h-full w-32 sm:w-48 bg-gradient-to-l from-transparent to-white dark:to-black z-10" />
+      <div className="absolute top-0 right-0 h-full w-32 sm:w-48 bg-gradient-to-r from-transparent to-white dark:to-black z-10" />
 
-        {/* Wrapping the logos in a container */}
-        <div className="relative w-screen overflow-x-hidden">
-          <div className="flex items-center space-x-10 animate-slide">
+      <div className="relative w-screen overflow-x-hidden">
+        <div className="flex items-center space-x-10 animate-slide">
+          {[
+            { src: "APLM.svg" },
+            { src: "djassaku.svg" },
+            { src: "AG.svg", invert: true },
+            { src: "Isomat.svg", invert: true },
+            { src: "Tedx.svg" },
+            { src: "Wajahni.png", invert: true },
+            { src: "flashBac.svg" },
+            { src: "Kreative.svg" },
+            { src: "creative.svg" },
+            { src: "AveMusic.svg" },
+            { src: "Spoy.svg" },
+            { src: "Artifex.svg", wide: true },
+            { src: "HvH.svg" },
+            { src: "hultprize.svg" },
+            { src: "TA.svg" },
+            { src: "APLM.svg" },
+            { src: "djassaku.svg" },
+            { src: "AG.svg", invert: true },
+            { src: "Isomat.svg", invert: true },
+            { src: "Tedx.svg" },
+            { src: "Wajahni.png", invert: true },
+            { src: "flashBac.svg" },
+            { src: "Kreative.svg" },
+            { src: "creative.svg" },
+            { src: "AveMusic.svg" },
+            { src: "Spoy.svg" },
+            { src: "Artifex.svg", wide: true },
+            { src: "HvH.svg" },
+            { src: "hultprize.svg" },
+            { src: "TA.svg" },
+          ].flatMap((logo, i) => [
             <img
-              src="/assets/images/logos/APLM.svg"
-              className="h-6 sm:h-8"
-              alt="APLM Logo"
+              key={`logo-${i}`}
+              src={`/assets/images/logos/${logo.src}`}
+              className={`h-6 sm:h-8 ${
+                logo.invert ? "invert dark:invert-0" : "dark:invert"
+              } ${logo.wide ? "w-24 sm:w-32 object-contain" : ""}`}
+              alt={`${logo.src.split(".")[0]} Logo`}
             />
-            <img
-              src="/assets/images/logos/djassaku.svg"
-              className="h-6 sm:h-8"
-              alt="djassaku Logo"
-            />
-            <img
-              src="/assets/images/logos/AG.svg"
-              className="h-6 sm:h-8 invert"
-              alt="accessgalleryofficial Logo"
-            />
-            <img
-              src="/assets/images/logos/Isomat.svg"
-              className="h-6 sm:h-8 invert"
-              alt="Isomat Logo"
-            />
-            <img
-              src="/assets/images/logos/Tedx.svg"
-              className="h-6 sm:h-8"
-              alt="Tedx Logo"
-            />
-            <img
-              src="/assets/images/logos/Wajahni.png"
-              className="h-6 sm:h-8 invert"
-              alt="Wajahni Logo"
-            />
-            <img
-              src="/assets/images/logos/flashBac.svg"
-              className="h-6 sm:h-8"
-              alt="flashBac Logo"
-            />
-            <img
-              src="/assets/images/logos/Kreative.svg"
-              className="h-6 sm:h-8"
-              alt="Kreative Logo"
-            />
-            <img
-              src="/assets/images/logos/creative.svg"
-              className="h-6 sm:h-8"
-              alt="creative Logo"
-            />
-            <img
-              src="/assets/images/logos/AveMusic.svg"
-              className="h-6 sm:h-8"
-              alt="AveMusic Logo"
-            />
-            <img
-              src="/assets/images/logos/TOUSIHEN.svg"
-              className="h-6 sm:h-8 invert"
-              alt="TOUSIHEN Logo"
-            />
-            <img
-              src="/assets/images/logos/Spoy.svg"
-              className="h-6 sm:h-8"
-              alt="Spoy Logo"
-            />
-            <img
-              src="/assets/images/logos/Artifex.svg"
-              className="h-6 sm:h-8 w-24 sm:w-32 object-contain"
-              alt="Artifex Logo"
-            />
-            <img
-              src="/assets/images/logos/HvH.svg"
-              className="h-6 sm:h-8"
-              alt="HvH Logo"
-            />
-            <img
-              src="/assets/images/logos/hultprize.svg"
-              className="h-6 sm:h-8"
-              alt="hultprize Logo"
-            />
-            <div className="h-6 sm:h-8 relative">
-              <div
-                className="absolute inset-0 h-full w-full bg-no-repeat bg-contain bg-center opacity-0 hover:opacity-100 transition-opacity duration-150"
-                style={{
-                  backgroundImage: `url('/assets/images/logos/menshealth.png')`,
-                }}
-              ></div>
-            </div>
-            <img
-              src="/assets/images/logos/TA.svg"
-              className="h-6 sm:h-8"
-              alt="TA Logo"
-            />
-            <img
-              src="/assets/images/logos/djassaku.svg"
-              className="h-6 sm:h-8"
-              alt="djassaku Logo"
-            />
-            <img
-              src="/assets/images/logos/APLM.svg"
-              className="h-6 sm:h-8"
-              alt="APLM Logo"
-            />
-            <img
-              src="/assets/images/logos/AG.svg"
-              className="h-6 sm:h-8 invert"
-              alt="accessgalleryofficial Logo"
-            />
-            <img
-              src="/assets/images/logos/Isomat.svg"
-              className="h-6 sm:h-8 invert"
-              alt="Isomat Logo"
-            />
-            <img
-              src="/assets/images/logos/Tedx.svg"
-              className="h-6 sm:h-8"
-              alt="Tedx Logo"
-            />
-            <img
-              src="/assets/images/logos/Wajahni.png"
-              className="h-6 sm:h-8 invert"
-              alt="Wajahni Logo"
-            />
-            <img
-              src="/assets/images/logos/flashBac.svg"
-              className="h-6 sm:h-8"
-              alt="flashBac Logo"
-            />
-            <img
-              src="/assets/images/logos/Kreative.svg"
-              className="h-6 sm:h-8"
-              alt="Kreative Logo"
-            />
-            <img
-              src="/assets/images/logos/creative.svg"
-              className="h-6 sm:h-8 w-24 sm:w-32 object-contain"
-              alt="creative Logo"
-            />
-            <img
-              src="/assets/images/logos/AveMusic.svg"
-              className="h-6 sm:h-8 w-24 sm:w-32 object-contain"
-              alt="AveMusic Logo"
-            />
-            <img
-              src="/assets/images/logos/Spoy.svg"
-              className="h-6 sm:h-8 w-24 sm:w-32 object-contain"
-              alt="Spoy Logo"
-            />
-            {/* Repeat the logos for continuous animation */}
-          </div>
+          ])}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
