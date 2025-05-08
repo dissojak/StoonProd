bad5da80886e828ef006a2a5d53aa42089051956
dissojak <dissojak@gmail.com>
2025-05-08 21:13:18 +0100

    fix sonarQube duplications services

--- src/app/serviceandtariffs/page.js
+++ src/app/serviceandtariffs/page.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Service from "../components/Services/Services";
 import MaintenanceSection from "../UI/MaintenanceSection";
+import Link from "next/link";
 
 function ServiceAndTariffs() {
   const eventCoverageService = {
@@ -149,9 +150,7 @@ function ServiceAndTariffs() {
       </Head>
 
       {/* Your component JSX code */}
-      <div className="absolute w-full z-50">
-        {/* <Navbar /> */}
-      </div>
+      <div className="absolute w-full z-50">{/* <Navbar /> */}</div>
       <div className="relative py-24 sm:py-32">
         <div className="block dark:hidden transition duration-1000">
           <Image
@@ -197,15 +196,15 @@ function ServiceAndTariffs() {
           </div>
           <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-              <a href="#">
+              <Link href="#">
                 Our Projects <span aria-hidden="true">&rarr;</span>
-              </a>
-              <a href="#">
+              </Link>
+              <Link href="#">
                 Contact Us <span aria-hidden="true">&rarr;</span>
-              </a>
-              <a href="#">
+              </Link>
+              <Link href="#">
                 Services Offered <span aria-hidden="true">&rarr;</span>
-              </a>
+              </Link>
             </div>
             <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
               <div className="flex flex-col-reverse">
