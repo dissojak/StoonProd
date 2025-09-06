f11d426591f635c7ff3a5b84f0ca8fe35c69551c
dissojak <dissojak@gmail.com>
2025-09-06 12:52:20 +0100

    just adding analyzer , some bug fixes

--- src/models/FigmaItem.js
+++ src/models/FigmaItem.js
@@ -4,7 +4,7 @@ const FigmaItemSchema = new mongoose.Schema({
   title: { type: String, required: true },
   link: { type: String, required: true },
   order: { type: Number, default: 0 },
-  image: { type: String, required: true },
+  image: { type: String, required: true }
 }, { timestamps: true });
 
 export default mongoose.models.FigmaItem || mongoose.model('FigmaItem', FigmaItemSchema);
