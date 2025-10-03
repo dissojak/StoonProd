0a598679557dcf5e31dd73f7f6821abb62e32855
dissojak <dissojak@gmail.com>
2025-10-03 14:18:54 +0100

    feat: Extract contact page constants

---
+++ src/app/contact/constants.ts
@@ -0,0 +1,8 @@
+export const CONTACT_DESCRIPTION = "We would love to hear from you! Whether you have a question about our services, want to start a project, or just want to say hello, our team is ready to help. Fill out the form and we&apos;ll get back to you as soon as possible.";
+export const ADDRESS_TEXT = "FR82+8V8, Al-Maamoura, Nabeul, Tunisia";
+export const PHONE_TEXT = "+216 23 039 320";
+export const REPLY_TEXT = "We reply within 24 hours";
+export const SUCCESS_TITLE = "Email Sent Successfully!";
+export const SUCCESS_BODY = "Your message was sent. Our team will respond within 24 hours.";
+export const ERROR_TITLE = "Error Sending Message";
+export const ERROR_BODY = "There was a problem sending your message. Please try again later or contact us directly.";
