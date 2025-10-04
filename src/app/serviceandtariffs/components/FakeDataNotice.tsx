7b59c04c73705a69cfa8383a56f876e23f6f6f25
dissojak <dissojak@gmail.com>
2025-10-04 15:06:23 +0100

    feat(service-tariffs): Add 'Fake Data Notice' component

---
+++ src/app/serviceandtariffs/components/FakeDataNotice.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FakeDataNotice: React.FC = () => (
+  <h1 className="text-4xl sm:text-5xl font-bold text-center text-red-600 dark:text-red-400 p-6">
+    🚨 All This Is Fake Data ! <br />
+    <span className="text-xl sm:text-2xl text-gray-700 dark:text-gray-300">Updates Coming Soon...</span>
+  </h1>
+);
+
+export default FakeDataNotice;
